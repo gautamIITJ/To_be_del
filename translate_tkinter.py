99e798385c4c542e01b43b98e5c40fd71906c934
Gautam Kumar <116097262+gautamIITJ@users.noreply.github.com>
2022-10-18 21:04:11 +0530

    Update translate_tkinter.py
    Improved the code and spacing

--- translate_tkinter.py
+++ translate_tkinter.py
@@ -2,12 +2,6 @@ NewLanguageChoiceMenu = OptionMenu(Screen,TranslateLanguageChoice,*LanguageChoic
 Label(Screen,text="Translated Language").grid(row=0,column=2)
 NewLanguageChoiceMenu.grid(row=1,column=2)
 
-Label(Screen,text="Enter Text").grid(row=2,column =0)
-TextVar = StringVar()
-TextBox = Entry(Screen,textvariable=TextVar).grid(row=2,column = 1)
-from tkinter import *
-from translate import Translator
-
 InputLanguageChoiceMenu = OptionMenu(Screen,InputLanguageChoice,*LanguageChoices)
 Label(Screen,text="Choose a Language").grid(row=0,column=1)
 InputLanguageChoiceMenu.grid(row=1,column=1)
